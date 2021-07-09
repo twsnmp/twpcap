734e89b8aeb5e6f925612b605fb45773a43c59ef
twsnmp <ymi@twise.co.jp>
2021-07-10 08:08:56 +0900

    DNSの問い合わせ情報をモニタ可能にした

--- pcap.go
+++ pcap.go
@@ -24,8 +24,30 @@ type IPToMACEnt struct {
 }
 
 func (e *IPToMACEnt) String() string {
-	return fmt.Sprintf("ip=%s,mac=%s,count=%d,change=%d,ft=%s,lt=%s",
+	return fmt.Sprintf("type=IPToMAC,ip=%s,mac=%s,count=%d,change=%d,ft=%s,lt=%s",
 		e.IP, e.MAC, e.Count, e.Change,
+		time.Unix(e.FirstTime, 0).Format(time.RFC3339),
+		time.Unix(e.LastTime, 0).Format(time.RFC3339),
+	)
+}
+
+type DNSEnt struct {
+	Type      string
+	Name      string
+	Count     int64
+	Change    int64
+	LastIP    string
+	LastMAC   string
+	FirstTime int64
+	LastTime  int64
+	SendTime  int64
+}
+
+func (e *DNSEnt) String() string {
+	return fmt.Sprintf("type=DNS,DNSType=%s,Name=%s,count=%d,change=%d,lastIP=%s,lastMAC=%s,ft=%s,lt=%s",
+		e.Type, e.Name, e.Count, e.Change,
+		e.LastIP,
+		e.LastMAC,
 		time.Unix(e.FirstTime, 0).Format(time.RFC3339),
 		time.Unix(e.LastTime, 0).Format(time.RFC3339),
 	)
@@ -34,6 +56,7 @@ func (e *IPToMACEnt) String() string {
 var (
 	IPToMAC   sync.Map
 	EtherType sync.Map
+	DNSQuery  sync.Map
 )
 
 func startPcap(ctx context.Context) {
@@ -67,6 +90,7 @@ func checkPacket(packet gopacket.Packet) {
 	if !ok {
 		return
 	}
+	macaddr := eth.SrcMAC.String()
 	updateEtherType(uint16(eth.EthernetType))
 	arpLayer := packet.Layer(layers.LayerTypeARP)
 	if arpLayer != nil {
@@ -81,26 +105,53 @@ func checkPacket(packet gopacket.Packet) {
 			net.HardwareAddr(arp.SourceHwAddress).String())
 		return
 	}
-	// ipLayer := packet.Layer(layers.LayerTypeIPv4)
-	// if ipLayer != nil {
-	// 	ip, ok := ipLayer.(*layers.IPv4)
-	// 	if !ok {
-	// 		return
-	// 	}
-	// 	updateIPToMAC(ip.SrcIP.String(), eth.SrcMAC.String())
-	// }
-	ipv6Layer := packet.Layer(layers.LayerTypeIPv6)
-	if ipv6Layer != nil {
-		ipv6, ok := ipv6Layer.(*layers.IPv6)
+	ipaddr := ""
+	ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
+	if ipv4Layer != nil {
+		ip, ok := ipv4Layer.(*layers.IPv4)
 		if !ok {
 			return
 		}
-		icmpv6NALayer := packet.Layer(layers.LayerTypeICMPv6NeighborAdvertisement)
-		icmpv6RALayer := packet.Layer(layers.LayerTypeICMPv6RouterAdvertisement)
-		if icmpv6NALayer != nil || icmpv6RALayer != nil {
-			updateIPToMAC(ipv6.SrcIP.String(), eth.SrcMAC.String())
+		ipaddr = ip.SrcIP.String()
+	} else {
+		ipv6Layer := packet.Layer(layers.LayerTypeIPv6)
+		if ipv6Layer != nil {
+			ipv6, ok := ipv6Layer.(*layers.IPv6)
+			if !ok {
+				return
+			}
+			ipaddr = ipv6.SrcIP.String()
+			icmpv6NALayer := packet.Layer(layers.LayerTypeICMPv6NeighborAdvertisement)
+			icmpv6RALayer := packet.Layer(layers.LayerTypeICMPv6RouterAdvertisement)
+			if icmpv6NALayer != nil || icmpv6RALayer != nil {
+				updateIPToMAC(ipaddr, macaddr)
+				return
+			}
+			if packet.Layer(layers.LayerTypeICMPv6) != nil {
+				return
+			}
+		}
+	}
+	// UDP
+	udpLayer := packet.Layer(layers.LayerTypeUDP)
+	if udpLayer != nil {
+		_, ok := udpLayer.(*layers.UDP)
+		if !ok {
 			return
 		}
+		dnsLayer := packet.Layer(layers.LayerTypeDNS)
+		if dnsLayer != nil {
+			dns, ok := dnsLayer.(*layers.DNS)
+			if !ok {
+				return
+			}
+			updateDNS(dns, ipaddr, macaddr)
+			return
+		}
+	}
+	// test
+	if eth.EthernetType == 0x8899 {
+		return
 	}
 
 	log.Println(packet)
@@ -114,38 +165,6 @@ func updateEtherType(t uint16) {
 	}
 	c++
 	EtherType.Store(t, c)
-}
-
-// syslogでレポートを送信する
-func sendReport() {
-	now := time.Now().Unix()
-	st := time.Now().Add(-time.Second * time.Duration(syslogInterval)).Unix()
-	rt := time.Now().Add(-time.Second * time.Duration(retentionData)).Unix()
-	IPToMAC.Range(func(k, v interface{}) bool {
-		if e, ok := v.(*IPToMACEnt); ok {
-			if e.LastTime < rt {
-				IPToMAC.Delete(e.IP)
-				return true
-			}
-			if e.SendTime < st {
-				syslogCh <- "type=IPToMAC," + e.String()
-				e.SendTime = now
-			}
-		}
-		return true
-	})
-	s := "type=EtherType"
-	EtherType.Range(func(key, value interface{}) bool {
-		if t, ok := key.(uint16); ok {
-			if c, ok := value.(int); ok {
-				s += fmt.Sprintf(",0x%04x=%d", t, c)
-			}
-		}
-		//レポートしたらクリアする
-		EtherType.Delete(key)
-		return true
-	})
-	syslogCh <- s
 }
 
 func updateIPToMAC(ip, mac string) {
@@ -166,5 +185,83 @@ func updateIPToMAC(ip, mac string) {
 		Count:     1,
 		FirstTime: now,
 		LastTime:  now,
+	})
+}
+
+func updateDNS(dns *layers.DNS, ip, mac string) {
+	if dns.OpCode != layers.DNSOpCodeQuery {
+		return
+	}
+	for _, q := range dns.Questions {
+		k := q.Type.String() + "," + string(q.Name)
+		log.Println(k)
+		if v, ok := DNSQuery.Load(k); ok {
+			if e, ok := v.(*DNSEnt); ok {
+				e.Count++
+				if mac != e.LastMAC || ip != e.LastIP {
+					e.Change++
+					e.LastMAC = mac
+					e.LastIP = ip
+				}
+				e.LastTime = time.Now().Unix()
+			}
+			continue
+		}
+		now := time.Now().Unix()
+		DNSQuery.Store(k, &DNSEnt{
+			Type:      q.Type.String(),
+			Name:      string(q.Name),
+			LastMAC:   mac,
+			LastIP:    ip,
+			Count:     1,
+			FirstTime: now,
+			LastTime:  now,
+		})
+	}
+}
+
+// syslogでレポートを送信する
+func sendReport() {
+	now := time.Now().Unix()
+	st := time.Now().Add(-time.Second * time.Duration(syslogInterval)).Unix()
+	rt := time.Now().Add(-time.Second * time.Duration(retentionData)).Unix()
+	IPToMAC.Range(func(k, v interface{}) bool {
+		if e, ok := v.(*IPToMACEnt); ok {
+			if e.LastTime < rt {
+				IPToMAC.Delete(e.IP)
+				return true
+			}
+			if e.SendTime < st {
+				syslogCh <- e.String()
+				e.SendTime = now
+			}
+		}
+		return true
+	})
+	s := "type=EtherType"
+	EtherType.Range(func(key, value interface{}) bool {
+		if t, ok := key.(uint16); ok {
+			if c, ok := value.(int); ok {
+				s += fmt.Sprintf(",0x%04x=%d", t, c)
+			}
+		}
+		//レポートしたらクリアする
+		EtherType.Delete(key)
+		return true
+	})
+	syslogCh <- s
+	// DNSレポート
+	DNSQuery.Range(func(k, v interface{}) bool {
+		if e, ok := v.(*DNSEnt); ok {
+			if e.LastTime < rt {
+				DNSQuery.Delete(k)
+				return true
+			}
+			if e.SendTime < st {
+				syslogCh <- e.String()
+				e.SendTime = now
+			}
+		}
+		return true
 	})
 }
